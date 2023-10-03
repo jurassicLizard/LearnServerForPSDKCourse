10b2738619f33b6902dbdf271b8fe93d5aa1b675
Salem Bouraoui <46156444+jurassicLizard@users.noreply.github.com>
2023-10-03 13:17:51 +0200

    Add Initial Project Skeleton

---
+++ src/main/java/com/triassic/systems/ps/dk/ng/gs/testserver/TestServerPsdkAngularCourseApplication.java
@@ -0,0 +1,13 @@
+package com.triassic.systems.ps.dk.ng.gs.testserver.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestServerPsdkAngularCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestServerPsdkAngularCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/triassic/systems/ps/dk/ng/gs/testserver/TestServerPsdkAngularCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triassic.systems.ps.dk.ng.gs.testserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestServerPsdkAngularCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
